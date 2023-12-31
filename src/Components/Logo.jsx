9adc2bbc064bf5e33945f9a62b5563f8734bce48
juanandreacchio <juan.andreacchio@gmail.com>
2023-12-30 22:37:25 -0300

    navbar corregido

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,7 +3,7 @@ import logoName from "/src/assets/hero/logoName.png";
 const Logo = () => {
   return (
     <a href="#" className=" z-50">
-      <div className=" w-20 z-20 ">
+      <div className=" w-[100px] z-20 ">
         {/* <RectangleGroupIcon /> */}
         <img src={logoName} alt="Logo San gabriel" />
       </div>
