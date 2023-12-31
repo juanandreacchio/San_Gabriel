0ea26694ccc79e6802796a1edb8c68e175a03bad
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2023-12-31 01:28:52 -0500

    Fusión cambios contacto y router dom

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,23 +14,31 @@ const Header = () => {
   const [isOpen, setisOpen] = useState(false);
 
   return (
-    <header className="bg-purple-header shadow-md text-white w-full p-2 text-2xl flex items-center z-50 h-altura_navbar_md font-manrope">
+    <header className="bg-purple-header shadow-md text-white w-full p-2 text-2xl flex items-center z-40 h-altura_navbar_md font-manrope ">
       <div className="py-4 px-7 w-full h-full flex flex-col md:flex-row justify-between items-start md:items-center contenedor">
         <Link to="/">
           <Logo />
         </Link>
-        <section className={` bg-purple-header md:bg-inherit flex flex-col md:flex-row items-center  justify-center gap-5  md:static absolute left-0 top-0  w-full h-full md:h-5 md:w-auto z-5 ease-in duration-800 transition-all md:translate-y-0  md:p-2 duration-300 ${
+        <section
+          className={` bg-purple-header md:bg-inherit flex flex-col md:flex-row items-center  justify-center gap-5  md:static absolute left-0 top-0  w-full h-full md:h-5 md:w-auto z-5 ease-in duration-800 transition-all md:translate-y-0  md:p-2 duration-300 ${
             isOpen
               ? "translate-y-0 "
               : "-translate-y-full opacity-0 md:opacity-100"
-          }`}>  
-            {links.map( link => (
-              <NavLink to={link.link} className={`my-5 md:my-0 mx-10 font-bold text-white-white_figma relative group md:text-md ${isOpen ? 'text-2xl' : 'text-base'}`} onClick={() => setisOpen(false)}>
-                <span>{link.name}</span>
-                <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white-white_figma transition-all group-hover:w-full"></span>
-              </NavLink>
-              
-            ))}
+          }`}
+        >
+          {links.map((link) => (
+            <NavLink
+              key={link.name}
+              to={link.link}
+              className={`my-5 md:my-0 mx-10 font-bold text-white-white_figma relative group md:text-md ${
+                isOpen ? "text-2xl" : "text-base"
+              }`}
+              onClick={() => setisOpen(false)}
+            >
+              <span>{link.name}</span>
+              <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white-white_figma transition-all group-hover:w-full"></span>
+            </NavLink>
+          ))}
         </section>
         <div
           onClick={() => setisOpen(!isOpen)}
--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -2,10 +2,10 @@ import logoName from "/src/assets/hero/logoName.png";
 
 const Logo = () => {
   return (
-    <a href="#" className=" z-50">
-      <div className=" w-[100px] z-20 ">
+    <a href="#" className="z-50">
+      <div className=" w-[100px] z-50 ">
         {/* <RectangleGroupIcon /> */}
-        <img src={logoName} alt="Logo San gabriel" />
+        <img src={logoName} alt="Logo San gabriel" className="z-50" />
       </div>
     </a>
   );
